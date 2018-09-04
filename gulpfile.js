f0e2921b67a94fdf905f492b387e0a9e0f29bf27
DIOGO SOARES <diogo@i9me.com.br>
2018-09-04 09:40:44 -0300

    Update Patch GulpFile.js

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ var sassProdOptions = {
 path = {
     dev     : './_src',
     prod    : './assets',
-    proxy   : 'http://localhost/barneys/franquia'
+    proxy   : 'http://localhost/geocron'
 }
 
 /*
