07c90e3426917a0b6f72bfffa056b5f137378157
Petrus Rennan <petrus@i9me.com.br>
2018-10-09 12:35:18 -0300

    para arredondar o form, use borderRadius100

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ get_header(); ?>
 	<?php get_template_part('template-parts/services/section','services') ?>
 	<?php get_template_part('template-parts/sobre/section','sobre') ?>
 	<?php get_template_part('template-parts/clientes/section','clientes') ?>
-	<?php get_template_part('template-parts/orcamento/section','orcamento') ?>
+	<?php get_template_part('template-parts/fale-conosco/section','fale-conosco') ?>
 	<?php get_template_part('template-parts/atendimento/section','atendimento') ?>
 </main><!-- #main -->
 
