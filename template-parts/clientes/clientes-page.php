5286eac3cb81257b3b8f74188d68ee8dd4ded844
Petruss <petrus@i9me.com.br>
2018-10-10 23:33:54 -0300

    estrutura html do loop criada

--- template-parts/clientes/clientes-page.php
+++ template-parts/clientes/clientes-page.php
@@ -3,13 +3,13 @@
 		<h3 class="Section-header-title Section-header-title--beforeTitleLine">Clientes</h3>
 		<p class="Section-header"></p>
 	</header>
-	<ul class="Section-items u-flexDirectionColumn u-displayFlex">
-		<li class="Section-items-item u-displayFlex u-flexDirectionColumn u-flexSwitchRow u-flexAlignItemsCenter u-flexJustifyContentCenter u-sizeFull u-paddingHorizontal--inter">
-			<figure class="Section-items-item-figure u-size12of24">
-				<img src="<?php echo get_template_directory_uri() ?>/assets/images/black.png">
+	<ul class="Section-items u-flexDirectionColumn u-displayFlex u-flexAlignItemsCenter u-flexJustifyContentCenter u-paddingVertical">
+		<li class="Section-items-item u-displayFlex u-flexDirectionColumn u-flexSwitchRow u-flexAlignItemsCenter u-flexJustifyContentCenter u-size20of24 u-paddingHorizontal--inter">
+			<figure class="Section-items-item-figure u-size10of24">
+				<img class="u-sizeFull u-height4of10" src="<?php echo get_template_directory_uri() ?>/assets/images/black.png">
 			</figure>
-			<div class="Section-items-item-content u-size12of24">
-				<h4 class="Section-items-item-content-title">Nome do Cliente</h4>
+			<div class="Section-items-item-content u-size16of24">
+				<h4 class="Section-items-item-content-title u-paddingBottom--inter--half">Nome do Cliente</h4>
 				<h5 class="Section-items-item-content-subtitle">Serviços prestados:</h5>
 				<p class="Section-items-item-content-resume">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
