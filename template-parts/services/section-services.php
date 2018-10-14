29c684009cfcdc9786f391b9958d5bff420d1807
Petruss <petrus@i9me.com.br>
2018-10-14 01:43:12 -0300

    estilizando a estrutura da section-home, compreendendo a estrutura.

--- template-parts/services/section-services.php
+++ template-parts/services/section-services.php
@@ -3,12 +3,12 @@
 		<h3 class="Section-header-title Section-header-title--beforeTitleLine u-alignCenter">Serviços</h3>
 	</header>
 	<ul class="Section-items u-sizeFull u-displayFlex u-flexDirectionColumn u-flexAlignItemsCenter">
-		<li class="Section-items-item u-displayFlex u-size16of24 u-flexDirectionColumn u-flexSwitchRow">
+		<li class="Section-items-item u-displayFlex u-size16of24 u-flexDirectionColumn u-flexSwitchRow u-paddingHorizontal--inter u-paddingVertical">
 			<div class="Section-items-item-content u-displayFlex u-size12of24 u-flexAlignItemsCenter">
-				<i class="FigureIcon FigureIcon--hidrogeologia u-marginLeft"></i>
+				<i class="FigureIcon FigureIcon--hidrogeologia"></i>
 				<h4 class="Section-items-item-content-title u-marginLeft">Hidrogeologia</h4>
 			</div>
-			<ul id="services-carousel" class="Section-items-item-list owl-carousel u-size12of24 u-marginRight">
+			<ul id="services-carousel" class="Section-items-item-list owl-carousel u-size12of24">
 				<li class="Section-items-item-list-point">
 					<figure class="Section-items-item-list-point-figure">
 						<div class="Section-items-item-list-point-black"></div>
